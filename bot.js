043a4f57bf57382f4969c0989d5e558681b34b25
Lava141 <Lava141@outlook.com>
2017-12-09 15:48:05 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on("ready", () => {
 
 client.on('message', message => {
     if (message.content === '!help') {
-      guildObj.defaultChannel.send('Here are all the commands you can execute');
+      message.send('Here are all the commands you can execute');
     	message.reply('Here are all the commands you can execute');
       message.reply('***!help :*** Displays all commands');
   	}
