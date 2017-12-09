b82ed9f0fcb4a091775ce4c3f53388c853d3b8ea
Lava141 <Lava141@outlook.com>
2017-12-09 15:53:54 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on("ready", () => {
 
 client.on('message', message => {
     if (message.content === '!help') {
-      message('Here are all the commands you can execute');
+      message.channel.send("Here are all the commands you can execute");
     	message.reply('Here are all the commands you can execute');
       message.reply('***!help :*** Displays all commands');
   	}
