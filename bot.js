170de4320fe5a1698085fa8e7490f224c2ac5935
Lava141 <Lava141@outlook.com>
2017-12-09 14:52:21 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === '!hello') {
+    	message.reply('Hello, my name is Rex Tracker and i will respond to all your questions and notify you went new updates arrive!');
   	}
 });
 
