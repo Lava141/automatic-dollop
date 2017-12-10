cb89ff79701ddbf64fe9d735ada9725796af317c
Lava141 <Lava141@outlook.com>
2017-12-10 10:45:05 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -146,6 +146,7 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === "!force-ads") {
+      message.delete();
       message.channel.send({embed});
     }
 });
