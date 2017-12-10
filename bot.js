a082f25bfeb21586c266ae14bb5f778ef8332d84
Lava141 <Lava141@outlook.com>
2017-12-10 09:41:45 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -131,23 +131,18 @@ client.on('message', message => {
 });
 
 const embed = new Discord.RichEmbed()
-  .setTitle("This is your title, it can hold 256 characters")
-  .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
   /*
    * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
    */
   .setColor(0x00AE86)
-  .setDescription("This is the main body of text, it can hold 2048 characters.")
-  .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
-  .setImage("http://i.imgur.com/yVpymuV.png")
-  .setThumbnail("http://i.imgur.com/p2qNFag.png")
+  .setThumbnail("https://i.imgur.com/8Gte59I.png")
   /*
    * Takes a Date object, defaults to current date.
    */
   .setTimestamp()
   .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
-  .addField("This is a field title, it can hold 256 characters",
-    "This is a field value, it can hold 2048 characters.")
+  .addField("[Ads]",
+    "Do you know about Rex Tracker? Its the brand new taming calculator / Ark Toolkit! http://rex-tracker.wcksoft.com")
   /*
    * Inline fields may not display as inline if the thumbnail and/or image is too big.
    */
