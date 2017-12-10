658410e9cdcf798d848ae1ca0c727640bdc56c67
Lava141 <Lava141@outlook.com>
2017-12-10 09:07:15 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -122,10 +122,10 @@ client.on('message', message => {
 });
 
 client.on('message', function() {
-    if (message.content === "$loop") { 
+    if (message.content === "!loop") { 
       var interval = setInterval (function () {
         message.channel.send("123")
-      }, 1 * 1000); 
+      }, 1 * 10); 
     }
 });
 
