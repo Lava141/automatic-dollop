de803fc8ca0760bdbe9a96f3c244ba52270d7a65
Lava141 <Lava141@outlook.com>
2017-12-22 19:32:53 -0400

    Update rex-restart.js

--- commands/rex-restart.js
+++ commands/rex-restart.js
@@ -1,6 +1,10 @@
 module.exports = {
   main: (bot, msg, settings) => {
-	bot.sendNotification(":x: You do not have permission to use this command.", "error", msg);
+	if (msg.author.id == bot.OWNERID) {
+		bot.sendNotification(":white_check_mark: Sucessfully restarted Rex Tracker!", "success", msg);
+	} else {
+		bot.sendNotification(":x: You do not have permission to use this command.", "error", msg);
+	}
   },
   args: '<string>',
   help: 'Restart Rex Tracker',
