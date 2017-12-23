51994766d96b2336200d74928bc6108b6655164d
Lava141 <Lava141@outlook.com>
2017-12-22 20:10:18 -0400

    Update hello.js

--- AI/hello.js
+++ AI/hello.js
@@ -1,1 +1,10 @@
-
+module.exports = {
+  main: (bot, msg, settings) => {
+	if (msg.author.id == bot.OWNERID) {
+		bot.sendNotification("Hello!", "success", msg);
+	}
+  },
+  args: '<string>',
+  help: '',
+  hide: false
+}
